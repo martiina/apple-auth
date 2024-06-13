576dc8255fd7a87e86f185de3e4c1796bc6293ec
Martin Mikson <martin@stagnationlab.com>
2024-06-13 18:06:12 +0300

    Fix stupid issues

--- src/apple-auth.js
+++ src/apple-auth.js
@@ -109,7 +109,10 @@ class AppleAuth {
                             reject("AppleAuth Error - An error occurred while getting response from Apple's servers: " + error + " - " + error?.response?.data?.error_description);
                         }
                         // If customConfig.debug isn't set, output in this format.                       
-                        const responseData = response.response?.data
+                        const responseData = error?.response?.data
+
+                        console.log({responseData});
+
                         reject(
                             `AppleAuth Error - An error occurred while getting response from Apple's servers: 
                             ${response}${responseData ? (" | " + responseData) : ""}`
